454e981f83c8171f9d031cff516deb1db22ce04d
ruszin <ruszmain@gmail.com>
2023-05-25 21:48:37 +0300

    test_end

--- yb_1w_sqr.cpp
+++ yb_1w_sqr.cpp
@@ -4,14 +4,9 @@
 #include<sstream>
 #include <string>
 #include <tuple>
-#include <cmath>
 
 using namespace std;
 
-template<typename T> vector<T> Sqr(vector<T> v);
-template<typename F, typename S> map<F, S> Sqr(map<F, S> v);
-template<typename F, typename S> pair<F, S> Sqr(pair<F, S> v);
-template<typename T> T Sqr(T v);
 
 template<typename F, typename S>
 map<F,S> Sqr(map<F,S> v) {
@@ -61,3 +56,39 @@ void main()
 }
 
 
+int main() {
+	3.   int a = 100;
+	4.   int b = 0;
+	5.   bool flag = getFlag();
+	6.   while (a > b) {
+		7.     if (flag) {
+			8.       b += 2;
+			9.       for (int i = 0; i <= calcCPU(a, b); i++) {
+				10.         int grep = initGrep(i + 1);
+				11.         b += grep % 2;
+				12.
+			}
+			13.       std::cout << a;
+			14.
+		}
+		15.     else {
+			16.       a -= 3;
+			17.       std::cout << b;
+			18.
+		}
+		19.
+			20.     if (a % 3 == 0) {
+			21.       std::cout << a;
+			22.
+		}
+		23.     else {
+			24.       a -= 2;
+			25.       std::cout << b;
+			26.
+		}
+		27.
+	}
+	28.   std::cout << "a = ", a, " b = ", b;
+	29.   return 0;
+	30.
+}
